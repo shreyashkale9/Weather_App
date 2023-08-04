5f875c4af876610432d0a629a00114594dd0d36d
Shreyash Kale <shreyashkale9@gmail.com>
2023-08-04 16:03:17 +0530

    First

---
+++ src/Components/CityComponent.js
@@ -0,0 +1,58 @@
+import styled from "styled-components"
+import React from "react"
+
+const WeatherLogo = styled.img`
+width:140px;
+height:140px;
+margin:40px auto;
+`
+const ChooseCityLabel = styled.span`
+color:black;
+font-size:18px;
+font-weight:bold;
+margin:10px auto;
+`
+
+const SearchBox = styled.form`
+display: flex;
+flex-direction: row;
+border: black solid 1px;
+border-radius: 2px;
+
+&input {
+    padding: 10px;
+    font-size: 14px;
+    border: none;
+    outline: none;
+    font-weight: bold;
+    font-family: 'PT Sans', sans-serif;
+}
+
+& button {
+    background-color: black;
+    font-size: 14px;
+    padding: 0 10px;
+    color: white;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    font-family: 'PT Sans', sans-serif;
+    font-weight: bold;
+  }
+`
+
+const CityComponent = (props) => {
+    const { updateCity, fetchWeather } = props;
+    return (
+        <>
+            <WeatherLogo src="Icons/weatherlogo.jpeg" />
+            <ChooseCityLabel>Enter City</ChooseCityLabel>
+            <SearchBox onSubmit={fetchWeather}>
+                <input placeholder="City" onChange={(e) => updateCity(e.target.value)}></input>
+                <button type="submit">Search</button>
+            </SearchBox>
+        </>
+    )
+}
+
+export default CityComponent;
